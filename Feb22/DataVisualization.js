4e977e4e4e09a1a930c527ce97e2f33a9df93bd2
kevinchu-mexiporc <98512579+kevinchu-mexiporc@users.noreply.github.com>
2022-02-21 22:14:36 +0400

    Update DataVisualization.js

--- Feb22/DataVisualization.js
+++ Feb22/DataVisualization.js
@@ -36,15 +36,14 @@ function setup() {
     "strings loaded from file successfully, read " + strings.length + " lines"
   );
 
-  // Find the minimum and maximum latitude
-  // and longitude
+  // Find the minimum and maximum latitude and longitude
   findMinMaxLatLong();
 }
 
 function findMinMaxLatLong() {
   let singleRow = [];
 
-  // loop over each row in the file
+  // loop over each row in the file from the very first line since I deleted the header
   for (let csvRowNumber = 0; csvRowNumber < strings.length; csvRowNumber++) {
     // get a single row and split that row
     // into individual words
@@ -67,6 +66,7 @@ function findMinMaxLatLong() {
         minLong = longitude - 3;
         maxLong = longitude + 3;
       }
+      // I rearrange the parameters of defining the min and max to 
 
       if (latitude < minLat) minLat = latitude;
       if (latitude > maxLat) maxLat = latitude;
@@ -86,33 +86,38 @@ function draw() {
   let singleRow = [];
 
   // get a single row and split that row into
-  // individual words
+  // individual strings
   singleRow = split(strings[csvRowNumber], ",");
 
-  // We know that all the fields I want in the file are are numerical:
+  // I know that all the fields I want in the file are are numerical:
   let longitude = float(singleRow[1]);
   let latitude = float(singleRow[2]);
   let depth = float(singleRow[3]);
   let magnitude = float(singleRow[4]);
-
+  // I got 4 fields from the CSV file
   // Check for non-numerical strings.
   if (isNaN(longitude) || isNaN(latitude)) {
     print("conversion to float failed; skipping row " + csvRowNumber);
   } else {
-    // scale that to fit on our canvas
+    // scale that to fit on the canvas
     let ypos = map(latitude, minLat, maxLat, 0, height);
     let xpos = map(longitude, minLong, maxLong, 0, width);
 
-    noStroke();
+    noStroke();//I don't want stroke on my data points
     // Put a mark there
     if (depth > 5) {
       fill(200 / depth + 220);
-    } else {
+    } 
+    // If the depth of the earthquake is not that shallow then 
+    // I will only display them in lighter color
+    else {
       fill(0);
     }
+    // If the depth of the earthquake is not that shallow then
+    // I will display it in black
 
     circle(xpos, ypos, magnitude * 13);
-  } // end of valid data
+  } // I will show the data points in the size of magnitude and here is the end of valid data
 
   csvRowNumber++;
   if (csvRowNumber >= strings.length) {
